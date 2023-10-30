7e4d82f36728f08e62bbe3abc3fb4260b1b7b70b
Daniel Tehrani <contact@dantehrani.com>
2023-10-30 21:31:53 +0900

    feat: plausible

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,16 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import '@/styles/globals.css';
+import type { AppProps } from 'next/app';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Script
+        defer
+        data-domain="traitcaster.vercel.app"
+        src="https://plausible.io/js/script.js"
+      ></Script>
+      <Component {...pageProps} />
+    </>
+  );
 }
