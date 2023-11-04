9dd1f29e224ab6271a98676bd2a8c96d58522cc5
Daniel Tehrani <contact@dantehrani.com>
2023-11-04 16:49:57 -0700

    feat: zora mint search results

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,6 +1,12 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
- 
-export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
-}
+import { type ClassValue, clsx } from 'clsx';
+import { twMerge } from 'tailwind-merge';
+
+export const cn = (...inputs: ClassValue[]) => {
+  return twMerge(clsx(inputs));
+};
+
+export const toIPFSGatewayUrl = (cid: string): string =>
+  `https://personae.infura-ipfs.io/ipfs/${cid}`;
+
+export const toZoraUrl = (contractAddress: string, tokenId: string): string =>
+  `https://zora.co/collect/zora:${contractAddress}/${tokenId}`;
